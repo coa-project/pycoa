628fd0edc3002a15ba2b71eb0c4c57ab5520a528
odadoun <odadoun@gmail.com>
2022-01-14 18:16:20 +0100

    convert GRC geo to epsg=4326

--- coa/geo.py
+++ coa/geo.py
@@ -1036,7 +1036,7 @@ class GeoCountry():
             self._country_data['name_region']=self._country_data['name_subregion']
             self._country_data['code_region']=self._country_data['code_subregion'] # no region info
             self._country_data=self._country_data[['name_subregion','code_subregion','population_subregion','name_region','code_region','geometry']]
-
+            self._country_data=self._country_data.to_crs(epsg=4326)
     # def get_region_from_municipality(self,lname):
     #     """  Return region list from a municipality list
     #     """

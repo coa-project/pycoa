47770d137b2421e7b46b15d8917f3efee879331e
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2022-02-11 12:13:12 +0100

    cosmetic

--- coa/dbinfo.py
+++ coa/dbinfo.py
@@ -130,6 +130,7 @@ def generic_info(namedb, keys):
     elif namedb == 'insee':
         insee = {
         'deaths_since_2018-01-01':['daily_number_of_deaths integrated since 2018-01-01 '],\
+        'daily_number_of_deaths':['daily_number_of_deaths'],\
         }
         for k,v in insee.items():
             insee[k].append('https://www.data.gouv.fr/fr/datasets/fichier-des-personnes-decedees/')

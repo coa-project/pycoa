9f80ab6ad4a01804a8a8a35bdbe79db027a0a262
odadoun <odadoun@gmail.com>
2024-12-18 07:48:32 +0100

    update example

--- examples/sentinelles.py
+++ examples/sentinelles.py
@@ -1,9 +1,12 @@
 # Example using sentinellesIRA database
-from src.front import bfront
+from src.front import front
 
 def test_pyvoa():
-    pyvoa = bfront()
-    pyvoa.setwhom('sentinellesIRA')
-    print(pyvoa.get())
+    pyvoa = front()
+    pyvoa.setvisu(vis='matplotlib')
+    pyvoa.setwhom('spf',reload=False)
+    pyvoa.map(mapoption='dense',tile='esri')
+    return pyvoa 
 
-pl = test_pyvoa()    
+pl = test_pyvoa()
+pl.savefig('mapspf.png')
